7d181536b9d8c40544f90a368e6a3c74af4ffc34
Yigit Mersin <yigitmersin@gmail.com>
2022-02-02 18:48:17 +0300

    Change fetchData func. & fetch single country and region data

--- src/redux/countries/countries.js
+++ src/redux/countries/countries.js
@@ -1,5 +1,5 @@
 const GET_DATA = 'covid-19-tracking-app/countries/GET_DATA';
-const baseURL = 'https://api.covid19tracking.narrativa.com/api/';
+const baseURL = 'https://api.covid19tracking.narrativa.com/api?date_from=';
 
 const initialState = [];
 
@@ -10,16 +10,28 @@ export const getData = (state) => ({
 
 export const fetchData = async (dispatch) => {
   const response = await fetch(
-    `https://api.covid19tracking.narrativa.com/api?date_from=${dateHelper()}&date_to=${dateHelper()}`,
+    baseURL + `${dateHelper()}&date_to=${dateHelper()}`,
   );
   const data = await response.json();
-  dispatch(getData(data));
+  const countryDatas = data.dates[dateHelper()].countries;
+  const dataValues = Object.entries(countryDatas);
+  const stateData = [];
 
-  const continents = await fetch('https://api.teleport.org/api/continents/');
-  const response2 = await continents.json();
+  dataValues.map((item, ind) => {
+    let country = {
+      name: item[0],
+      id: item[1].id,
+      newCase: item[1].today_confirmed,
+      total: item[1].today_new_confirmed,
+      regions: {
+        region: item[1].regions,
+      },
+    };
+    stateData.push(country);
+  });
 
-  dispatch(getData(response2._links['continent:items']));
-  //loop line 21, add /countries to end
+  dispatch(getData(stateData));
+};
 
 export const dateHelper = () => {
   let date = new Date();
