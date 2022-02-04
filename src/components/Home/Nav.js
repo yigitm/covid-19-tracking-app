6c4a10e23e7b28c24147f3b567ca89a82f3b7d00
Yigit Mersin <yigitmersin@gmail.com>
2022-02-04 10:20:51 +0300

    Adjust mid & mobile screen fonts

--- src/components/Home/Nav.js
+++ src/components/Home/Nav.js
@@ -6,14 +6,14 @@ const Nav = () => (
   <Navbar className="m-0 p-0 bg-red-bold">
     <Navbar.Brand>
       <Link to="/">
-        <i className="fas fa-angle-left icon text-light" />
+        <i className="fas fa-angle-left icon-nav text-light" />
       </Link>
     </Navbar.Brand>
     <Navbar.Toggle />
     <Navbar.Collapse className="justify-content-end">
       <Navbar.Text>
-        <i className="fas fa-microphone icon text-light mx-5" />
-        <i className="fas fa-cog icon text-light" />
+        <i className="fas fa-microphone icon-nav text-light mx-5" />
+        <i className="fas fa-cog icon-nav text-light" />
       </Navbar.Text>
     </Navbar.Collapse>
   </Navbar>
