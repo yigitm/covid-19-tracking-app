7e4a1127d4947fa193ff21d6e3ede3fe1cf47ac5
Yigit Mersin <yigitmersin@gmail.com>
2022-02-04 16:36:16 +0300

    Add loading to no region render

--- src/components/Detail/Noregion.js
+++ src/components/Detail/Noregion.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
+import Loading from '../Home/Loading';
 
 const noRegion = () => (
   <Col className="col-12 bg-red">
+    <Loading />
     <Card.Body className="d-flex justify-content-between">
       <i className="fas fa-chart-pie text-left text-light icon-header" />
 
