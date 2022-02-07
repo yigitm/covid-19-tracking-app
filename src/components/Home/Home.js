1d32f20a065a9cc2de46b71dba483c9393ed6288
Yigit Mersin <yigitmersin@gmail.com>
2022-02-08 00:45:21 +0300

    Change meta tag & add useeffect to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,27 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Container, Row } from 'react-bootstrap';
 import Loading from './Loading';
 import Nav from './Nav';
 import Header from './Header';
 import Countries from './Countries';
+import { fetchData } from '../../redux/countries/countries';
 
-const Home = () => (
-  <Container fluid className="bg-red-bold">
-    <Loading />
-    <Nav />
-    <Header />
-    <Row>
-      <Countries />
-    </Row>
-  </Container>
-);
+const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchData);
+  }, []);
+  return (
+    <Container fluid className="bg-red-bold">
+      <Loading />
+      <Nav />
+      <Header />
+      <Row>
+        <Countries />
+      </Row>
+    </Container>
+  );
+};
 
 export default Home;
